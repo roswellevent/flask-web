e986de6a153f3c76175d7115d8352cb54fdc25d2
Roswellevent@Desktop <roswellevent@gmail.com>
2020-08-02 12:35:55 +0800

    add test case

--- test_program.py
+++ test_program.py
@@ -8,12 +8,13 @@ class TestHello(unittest.TestCase):
         app.app.testing = True
         self.app = app.app.test_client()
 
-    def test_hello(self):
+    def test_case1(self):
         rv = self.app.get('/')
         self.assertEqual(rv.status, '200 OK')
+
+    def test_case2(self):
+        rv = self.app.get('/')
         self.assertEqual(rv.data, b'This is a simple Python Web for Demo Use')
 
-    def test_hello2(self):
-        rv = self.app.get('/test')
-        self.assertEqual(rv.status, '200 OK')
+
 
